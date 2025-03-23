c93686131b22aa85d869a2be17b82308479843b3
joaoprado-rs <joaoprado275@gmail.com>
2025-03-23 00:14:45 -0300

    Implementing new route

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,17 @@ use actix_web::{
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
+#[derive(Serialize, Deserialize)]
+struct Error {
+    reason: String,
+    message: String,
+}
+
+#[derive(Serialize, Deserialize)]
+struct Errors {
+    errors: Vec<Error>,
+}
+
 #[derive(Serialize, Deserialize, Clone)]
 struct User {
     id: Uuid,
@@ -35,16 +46,16 @@ struct AppState {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    print!("Starting service at port 8080...");
     let app = web::Data::new(AppState {
         users: Mutex::new(vec![]),
     });
-
-    print!("Starting service at port 8080...");
     HttpServer::new(move || {
         App::new()
             .app_data(app.clone())
             .service(insert_user)
             .service(get_users)
+            .service(get_user)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
@@ -105,4 +116,52 @@ async fn get_users(data: web::Data<AppState>) -> impl Responder {
     HttpResponse::Ok()
         .content_type(ContentType::json())
         .body(request_response)
+}
+
+#[get("/users/{id}")]
+async fn get_user(id: web::Path<String>, data: web::Data<AppState>) -> impl Responder {
+    let users = data.users.lock().unwrap();
+    let uuid = match Uuid::parse_str(&id) {
+        Ok(uuid) => uuid,
+        Err(_) => {
+            let mut vec_err: Vec<Error> = Vec::new();
+            vec_err.push(Error {
+                reason: String::from("Bad Request"),
+                message: String::from("Invalid UUID format in the path"),
+            });
+            return HttpResponse::NotFound()
+                .content_type(ContentType::json())
+                .body(serde_json::to_string(&Errors { errors: vec_err }).unwrap());
+        }
+    };
+
+    let response: HttpResponse = match users.iter().find(|user| user.id == uuid) {
+        Some(user) => {
+            let user_response = UserResponse {
+                id: user.id,
+                username: user.username.clone(),
+                is_active: user.is_active,
+            };
+            HttpResponse::NotFound()
+                .content_type(ContentType::json())
+                .body(serde_json::to_string(&user_response).unwrap())
+        }
+        None => {
+            let error_message = Error {
+                reason: String::from("user not found"),
+                message: format!(
+                    "The user {} was not found in the management. Try using `is_active` = false in the query parameter",
+                    uuid
+                ),
+            };
+            let mut vec_errors: Vec<Error> = Vec::new();
+            vec_errors.push(error_message);
+            let errors = Errors { errors: vec_errors };
+
+            HttpResponse::NotFound()
+                .content_type(ContentType::json())
+                .body(serde_json::to_string(&errors).unwrap())
+        }
+    };
+    response
 }
